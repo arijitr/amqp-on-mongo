52bbcf8c101d2ed29a4d60c11ba02dbfc4654cf9
arijitr <66120014+arijitr@users.noreply.github.com>
2020-12-01 21:24:45 +0530

    Update subscriber.js

--- subscriber.js
+++ subscriber.js
@@ -1,3 +1,4 @@
+// A sample subscriber to show how to use the broker to listen messages from publisher.
 const Broker=require('./broker');
 const MONGO_URL='mongodb://localhost:27017?authSource=admin';
 let options={
@@ -5,6 +6,7 @@ let options={
     dbname: "broker",
     name: "StockMarket"
 }
+//create a broker passing the option parameter which have the url for the Mongo .
 Broker.create(options).then(async (broker)=>{
     broker.subscribe('bse',{routingkey:"BSE",autoAck:false},(data)=>{
         let datetime=new Date();
